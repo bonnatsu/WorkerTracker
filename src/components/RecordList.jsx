7fbdbde804fa9884d6afcd15445a8d42b93aca7a
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-03-28 00:41:29 +0900

    modify

--- src/components/RecordList.jsx
+++ src/components/RecordList.jsx
@@ -5,7 +5,7 @@ function RecordList({ records }) {
       <ul>
         {records.map((r) => (
           <li key={r.id}>
-            {r.employeeName} / {r.category}-{r.subCategory}：
+            {r.employeeName} / {r.category}-{r.subcategory}：
             {r.startTime.toLocaleTimeString()} 〜{" "}
             {r.endTime ? r.endTime.toLocaleTimeString() : "作業中"}
           </li>
