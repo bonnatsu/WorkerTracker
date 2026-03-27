e50c6bfbbc25d4d9a196ae2909e863f6b8be71d6
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-03-28 00:37:11 +0900

    modify

--- src/components/WorkSelector.jsx
+++ src/components/WorkSelector.jsx
@@ -1,14 +1,14 @@
-function WorkSelector({ category, setCategory, subCategory, setSubCategory, worklist }) {
+function WorkSelector({ category, setcategory, subcategory, setSubcategory, worklist }) {
   return (
     <div>
-      <select onChange={(e) => setCategory(e.target.value)} value={category}>
+      <select onChange={(e) => setcategory(e.target.value)} value={category}>
         <option value="">大項目</option>
         {Object.keys(worklist).map((key) => (
           <option key={key} value={key}>{key}</option>
         ))}
       </select>
 
-      <select onChange={(e) => setSubCategory(e.target.value)} value={subCategory}>
+      <select onChange={(e) => setSubcategory(e.target.value)} value={subcategory}>
         <option value="">小項目</option>
         {category && worklist[category].map((sub) => (
           <option key={sub} value={sub}>{sub}</option>
