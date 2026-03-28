8aa926ecdc2f52313111bf1f589afc913d6cd3d2
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-03-28 16:15:51 +0900

    modify

---
+++ src/components/UserMaster.jsx
@@ -0,0 +1,63 @@
+import { useEffect,useState } from "react";
+import {supabase} from "..lib/supabase";
+
+
+function UsersMaster() {
+    const [users, setUsers] = useState([]);
+    const [name,setName] = useState([]);
+
+    const fetchUsers = async () => {
+        const {data, error} = await supabase.from("users").select("*");
+
+        if (error) {
+            console.error(error);
+        } else {
+            setUsers(data);
+        }
+    };
+}
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const handleAdd = async () => {
+        const {error} = await supabase.from("users").insert([{name}]);
+
+        if (!error) {
+            setName("");
+            fetchUsers();
+        }
+    };
+
+    const handleDelete = async (id) => {
+        await supabase.from("users").delete().eq("id",id);
+        fetchUsers();
+    };
+
+
+    return (
+        <div>
+            <h2>ユーザマスタ</h2>
+
+
+            <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="名前"
+            />
+            <button onClick={handleAdd}>追加</button>
+
+            <ul>
+                {users.map((u) => (
+                    <li key={u.id}>
+                        {u.name}
+                        <button onClick={() => handleDelete(u.id)}>削除</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+
+
+    export default UsersMaster;
