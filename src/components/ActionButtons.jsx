4b1ca132a3550678f152281d60fc5a9ff0e03d39
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-03-29 22:45:52 +0900

    modify

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -1,13 +1,19 @@
 
 
 function ActionButtons({ onStart, onEnd, onAllEnd,onOpenMaster,onOpenWorkMaster }) {
+  const btnStyle = {
+    fontSize: "18px",
+    padding: "16px",
+    minHeight: "60px",
+    borderRadius: "10px"
+  }
   return (
-    <div style={{ marginTop: "1rem" }}>
-      <button onClick={onStart}>作業開始</button>
-      <button onClick={onEnd}>作業終了</button>
-      <button onClick={onAllEnd}>一括終了</button>
-      <button onClick={onOpenMaster}>ユーザマスタ編集</button>
-      <button onClick={onOpenWorkMaster}>作業項目編集</button>
+    <div style={{ display:"flex",flexDirection:"column",gap:"16px" }}>
+      <button style={btnStyle} onClick={onStart}>作業開始</button>
+      <button style={btnStyle} onClick={onEnd}>作業終了</button>
+      <button style={btnStyle} onClick={onAllEnd}>一括終了</button>
+      <button style={btnStyle} onClick={onOpenMaster}>ユーザマスタ編集</button>
+      <button style={btnStyle} onClick={onOpenWorkMaster}>作業項目編集</button>
     </div>
   );
 }
