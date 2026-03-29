2bd13dfbb87014b550e9f1537b070697b4bc0523
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-03-29 23:03:04 +0900

    modify

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -2,22 +2,43 @@
 
 function ActionButtons({ onStart, onEnd, onAllEnd,onOpenMaster,onOpenWorkMaster }) {
   const btnStyle = {
-    width: "100%",
+    width: "140px",
     fontSize: "18px",
     padding: "16px",
     marginBottom: "16px",
     height: "60px",
     borderRadius: "10px"
-  }
+  };
+
+  const subBtn = {
+    width: "200px",
+    height:"50px",
+    fontSize:"16px",
+    marginBottom:"12px",
+    borderRadius:"10px"
+  };
+
+  const flexMenu = {
+    position: "flexed",
+    bottom: "20px",
+    right: "20px",
+    display: "flex",
+    flexDirection:"column",
+  };
   return (
-    <div style={{ display:"flex",flexDirection:"column",gap:"16px" }}>
-      <button style={btnStyle} onClick={onStart}>作業開始</button>
-      <button style={btnStyle} onClick={onEnd}>作業終了</button>
-      <button style={btnStyle} onClick={onAllEnd}>一括終了</button>
-      <button style={btnStyle} onClick={onOpenMaster}>ユーザマスタ編集</button>
-      <button style={btnStyle} onClick={onOpenWorkMaster}>作業項目編集</button>
+    <div>
+      <div style={{ display:"flex",flexDirection:"column",gap:"16px" }}>
+        <button style={btnStyle} onClick={onStart}>作業開始</button>
+        <button style={btnStyle} onClick={onEnd}>作業終了</button>
+      </div>
+      <div style={flexMenu}>
+        <button style={subBtn} onClick={onAllEnd}>一括終了</button>
+        <button style={subBtn} onClick={onOpenMaster}>ユーザマスタ編集</button>
+        <button style={subBtn} onClick={onOpenWorkMaster}>作業項目編集</button>
+      </div>
     </div>
   );
+
 }
 
 export default ActionButtons;
