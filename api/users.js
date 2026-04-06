b5c2a680706141d984a50a4e4feb0fd52bd2b12f
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-04-06 23:46:51 +0900

    modify

--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(
-    process.env.VITE_SUPABASE_URL,
-    process.env.VITE_SUPABASE_ANON_KEY
+    process.env.supabaseUrl,
+    process.env.supabaseKey 
 );
 
 export default async function handler(req, res) {
