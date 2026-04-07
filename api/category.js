d2671f5269d8dc2ba459dc2d1c180a69bbeaf915
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-04-07 23:44:44 +0900

    modify

--- api/category.js
+++ api/category.js
@@ -27,6 +27,6 @@ export default async function handler(req, res) {
             return res.status(500).json({ error: error.message });
         }
 
-        res.status(200).json([{ data }]);
+        res.status(200).json( data );
     }
 }
