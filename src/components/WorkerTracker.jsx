76656d8507a86e68b844300d5feca0181ac332e3
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-04-08 23:13:58 +0900

    modify

--- src/components/WorkerTracker.jsx
+++ src/components/WorkerTracker.jsx
@@ -1,8 +1,4 @@
 import { useState } from "react";
-import UserInput from './UserInput';
-import WorkSelector from './WorkSelector';
-import ActionButtons from './ActionButtons';
-import RecordList from './RecordList';
 import { supabase } from '../lib/supabase';
 import { useEffect } from "react";
 import UsersMaster from "../master/UserMaster";
