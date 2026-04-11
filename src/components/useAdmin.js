02a232ae97c2d01b58f82408d059c9a5d81e205c
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-04-11 23:44:31 +0900

    modify

--- src/components/useAdmin.js
+++ src/components/useAdmin.js
@@ -4,6 +4,7 @@ export const useAdmin = () => {
     const withAdmin = (action) => async () => {
         const ok = await checkAdmin();
         if (!ok) return;
+        action();
     };
 
     return { withAdmin };
